fdb72d4165e94762863dce400090669010611025
Renan <renan.baldo@sou.unifeob.edu.br>
2019-05-22 11:49:17 -0300

    Entrega dos Arquivos e PrintScreens

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 
+import bookRoutes from './routes/BookRoutes'
+
 const app = express()
 const BASE_URI = '/api/v1'
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(BASE_URI, bookRoutes)
 
 app.get('*', (req, res) => {
   res.status(200).send({messsage: 'Welcome to this API'})
